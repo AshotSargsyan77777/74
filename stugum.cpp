430d51d6146d63117cbe376ac73890a71f59efae
Ashot Sargsyan <ashotsargsyan74122@gmail.com>
2026-01-08 00:40:33 +0400

    a

--- stugum.cpp
+++ stugum.cpp
@@ -1,6 +1,6 @@
 #include "stugum.h"
 #include "animal.h"
 
-bool isAnimal(const Animal* obj) {
-    return dynamic_cast<const Animal*>(obj) != nullptr;
+bool isAnimal( Animal* obj) {
+    return dynamic_cast< Animal*>(obj) != nullptr;
 }
